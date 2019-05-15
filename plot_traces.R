834f733bae3d100849d07d21e95a3b81d0598d38
Rich <plethoraofchutneys@gmail.com>
2019-05-15 16:18:14 -0700

    Clip trace outside view limits for better y-lims

--- plot_traces.R
+++ plot_traces.R
@@ -9,16 +9,16 @@ max_frac <- as.integer(args[3])
 low_ml <- as.integer(args[4])
 high_ml <- as.integer(args[5])
 
-data <- read_csv(args[1], col_types = 'dcddcci') %>% 
+data <- read_csv(args[1], col_types = 'dcddcci') %>%
   mutate(inst_frac = if_else(inst_frac < min_frac, 'Waste', if_else(inst_frac > max_frac, 'Waste', as.character(inst_frac))))
 
 if (length(levels(factor(data$Sample))) > 1) {
 data %>%
-  filter(Channel == 'mAU' & mL > low_ml & mL < high_ml) %>% 
-  group_by(Sample) %>% 
-  mutate(Normalized = ((Signal - min(Signal)) / (max(Signal) - min(Signal)))) %>% 
-  gather(key = Normalized, value = Signal, Signal, Normalized) %>% 
-  ungroup() %>% 
+  filter(Channel == 'mAU' & mL > low_ml & mL < high_ml) %>%
+  group_by(Sample) %>%
+  mutate(Normalized = ((Signal - min(Signal)) / (max(Signal) - min(Signal)))) %>%
+  gather(key = Normalized, value = Signal, Signal, Normalized) %>%
+  ungroup() %>%
   ggplot(aes(x = mL, y = Signal, color = Sample)) +
   facet_grid(Normalized ~ ., scales = 'free') +
   theme_light() +
@@ -27,6 +27,7 @@ data %>%
 ggsave(filename = file.path(out.dir, paste('all_samples_', no.ext, '.pdf', sep = '')), width = 8, height = 5)
 } else {
   data %>%
+    filter(mL > (low_ml - 10) & mL < (high_ml + 10)) %>%
     ggplot(aes(x = mL, y = Signal, color = Channel)) +
     theme_light() +
     coord_cartesian(xlim = c(low_ml, high_ml)) +
@@ -36,9 +37,10 @@ ggsave(filename = file.path(out.dir, paste('all_samples_', no.ext, '.pdf', sep =
 }
 
 if (max_frac == 0) {
-  data %>% 
-    filter(Channel == 'mAU') %>% 
-    group_by(Sample) %>% 
+  data %>%
+    filter(Channel == 'mAU') %>%
+    filter(mL > (low_ml - 10) & mL < (high_ml + 10)) %>%
+    group_by(Sample) %>%
     ggplot() +
     coord_cartesian(xlim = c(low_ml, high_ml)) +
     theme_light() +
@@ -48,9 +50,10 @@ if (max_frac == 0) {
 }
 
 if (max_frac > 0) {
-  data %>% 
-    filter(Channel == 'mAU') %>% 
-    group_by(Sample) %>% 
+  data %>%
+    filter(Channel == 'mAU') %>%
+    filter(mL > (low_ml - 10) & mL < (high_ml + 10)) %>%
+    group_by(Sample) %>%
     ggplot() +
     coord_cartesian(xlim = c(low_ml, high_ml)) +
     theme_light() +
@@ -61,4 +64,3 @@ if (max_frac > 0) {
     facet_grid(Sample ~ ., scales = 'free')
   ggsave(filename = file.path(out.dir, paste('mAU_fractions_', no.ext, '.pdf', sep = '')), width = 6, height = 4)
 }
-
